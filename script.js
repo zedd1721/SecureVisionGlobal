9e640c1879bb677e5bf64ec846b7fe1320f9f63b
Zain Shaikh <szain2024@gmail.com>
2024-01-22 08:08:28 +0530

    Homepage in Progress

---
+++ script.js
@@ -0,0 +1,22 @@
+// function showSideBar(){
+//     const sidebar = document.querySelector(".sidebar");
+//     // sidebar.style.display = 'flex'
+//     sidebar.style.right= "0px";
+// }
+
+// function hideSideBar(){
+//     const sidebar = document.querySelector(".sidebar");
+//     // sidebar.style.display = 'none';
+//     sidebar.style.right= "-300px"
+// }
+
+const opensidebar = document.querySelector("#menu-btn")
+const sidebar = document.querySelector('.sidebar')
+opensidebar.addEventListener('click', function(){
+    sidebar.style.right= "0px"
+    // sidebar.style.display ='flex'
+})
+const closesidebar = document.querySelector('#hidesidebar')
+closesidebar.addEventListener('click', function(){
+    sidebar.style.right= "-300px"
+})
