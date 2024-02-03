2db944235f279af8a8f4190c1b697348fe5ed74c
Zain Shaikh <szain2024@gmail.com>
2024-02-03 16:46:18 +0530

    About page completed and pages linked

--- script.js
+++ script.js
@@ -12,7 +12,17 @@ var swiper = new Swiper(".mySwiper", {
   const sidetesti = document.querySelector('#sidetesti');
   sidetesti.addEventListener('click', ()=>{
     window.location.href = 'testimonial.html';
-    }) 
+    })
+
+    const about = document.querySelector('#about');
+    about.addEventListener('click', ()=>{
+        window.location.href = 'about.html';
+    })
+    
+    const sideabout = document.querySelector('#sideabout')
+    sideabout.addEventListener('click', ()=>{
+        window.location.href = 'about.html';
+    })
 const opensidebar = document.querySelector("#menu-btn")
 const sidebar = document.querySelector('.sidebar')
 opensidebar.addEventListener('click', function(){
