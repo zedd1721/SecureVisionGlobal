36a5b943e5b61fbff7f6667d853e551941881489
Zain Shaikh <szain2024@gmail.com>
2024-02-09 00:16:34 +0530

    Jobs Page added

--- script.js
+++ script.js
@@ -1,56 +1,66 @@
 var swiper = new Swiper(".mySwiper", {
-    effect: "cards",
-    grabCursor: true,
-    autoplay: true,
-  });
+  effect: "cards",
+  grabCursor: true,
+  autoplay: true,
+});
 
-  const home = document.getElementById("home");
-  home.addEventListener("click", () => {
-    window.location.href = "index.html";
-  });
-  
-  const sidehome = document.querySelector("#sidehome");
-  sidehome.addEventListener("click", () => {
-    window.location.href = "index.html";
-  });
+const home = document.getElementById("home");
+home.addEventListener("click", () => {
+  window.location.href = "index.html";
+});
 
-  const testimonial = document.querySelector('#testimonials');
-  testimonial.addEventListener('click', ()=>{
-      window.location.href = 'testimonial.html';
-  })    
+const sidehome = document.querySelector("#sidehome");
+sidehome.addEventListener("click", () => {
+  window.location.href = "index.html";
+});
 
-  const sidetesti = document.querySelector('#sidetesti');
-  sidetesti.addEventListener('click', ()=>{
-    window.location.href = 'testimonial.html';
-    })
+const testimonial = document.querySelector("#testimonials");
+testimonial.addEventListener("click", () => {
+  window.location.href = "testimonial.html";
+});
 
-    const about = document.querySelector('#about');
-    about.addEventListener('click', ()=>{
-        window.location.href = 'about.html';
-    })
-    
-    const sideabout = document.querySelector('#sideabout')
-    sideabout.addEventListener('click', ()=>{
-        window.location.href = 'about.html';
-    })
+const sidetesti = document.querySelector("#sidetesti");
+sidetesti.addEventListener("click", () => {
+  window.location.href = "testimonial.html";
+});
 
-    const contact = document.querySelector('#contact');
-    contact.addEventListener('click', ()=>{
-        window.location.href = 'contact.html';
-    })    
+const jobs = document.getElementById("jobs");
+jobs.addEventListener("click", () => {
+  window.location.href = "jobs.html";
+});
 
-    const sidecontact = document.querySelector('#sidecontact');
-    sidecontact.addEventListener('click', ()=>{
-        window.location.href = 'contact.html';
-    }) 
+const sidejobs = document.querySelector("#sidejobs");
+sidejobs.addEventListener("click", () => {
+  window.location.href = "jobs.html";
+});
 
-    const opensidebar = document.querySelector("#menu-btn")
-    const sidebar = document.querySelector('.sidebar')
-    opensidebar.addEventListener('click', function(){
-        sidebar.style.right= "0px"
-        // sidebar.style.display ='flex'
-    })
-    const closesidebar = document.querySelector('#hidesidebar')
-    closesidebar.addEventListener('click', function(){
-        sidebar.style.right= "-100%"
-    })
+const about = document.querySelector("#about");
+about.addEventListener("click", () => {
+  window.location.href = "about.html";
+});
+
+const sideabout = document.querySelector("#sideabout");
+sideabout.addEventListener("click", () => {
+  window.location.href = "about.html";
+});
+
+const contact = document.querySelector("#contact");
+contact.addEventListener("click", () => {
+  window.location.href = "contact.html";
+});
+
+const sidecontact = document.querySelector("#sidecontact");
+sidecontact.addEventListener("click", () => {
+  window.location.href = "contact.html";
+});
+
+const opensidebar = document.querySelector("#menu-btn");
+const sidebar = document.querySelector(".sidebar");
+opensidebar.addEventListener("click", function () {
+  sidebar.style.right = "0px";
+  // sidebar.style.display ='flex'
+});
+const closesidebar = document.querySelector("#hidesidebar");
+closesidebar.addEventListener("click", function () {
+  sidebar.style.right = "-100%";
+});
